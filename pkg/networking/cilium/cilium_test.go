d852e20aaa024cb69994c233c1452e86d1448441
Rajashree Mandaogane <mandaor@amazon.com>
2022-02-15 11:41:00 -0800

    Use helm template to generate cilium manifest

--- pkg/networking/cilium/cilium_test.go
+++ pkg/networking/cilium/cilium_test.go
@@ -11,7 +11,6 @@ import (
 	"github.com/aws/eks-anywhere/pkg/cluster"
 	"github.com/aws/eks-anywhere/pkg/networking/cilium"
 	"github.com/aws/eks-anywhere/pkg/networking/cilium/mocks"
-	"github.com/aws/eks-anywhere/release/api/v1alpha1"
 )
 
 type ciliumtest struct {
@@ -33,17 +32,7 @@ func newCiliumTest(t *testing.T) *ciliumtest {
 		client: *client,
 		h:      h,
 		spec: test.NewClusterSpec(func(s *cluster.Spec) {
-			s.VersionsBundle.Cilium = v1alpha1.CiliumBundle{
-				Cilium: v1alpha1.Image{
-					URI: "public.ecr.aws/isovalent/cilium:v1.9.10-eksa.1",
-				},
-				Operator: v1alpha1.Image{
-					URI: "public.ecr.aws/isovalent/operator-generic:v1.9.10-eksa.1",
-				},
-				Manifest: v1alpha1.Manifest{
-					URI: "testdata/cilium_manifest.yaml",
-				},
-			}
+			s.VersionsBundle.Cilium.Version = "v1.9.11-eksa.1"
 		}),
 		cilium: cilium.NewCilium(client, h),
 	}
@@ -51,16 +40,11 @@ func newCiliumTest(t *testing.T) *ciliumtest {
 
 func TestCiliumGenerateManifestSuccess(t *testing.T) {
 	tt := newCiliumTest(t)
-
-	gotFileContent, err := tt.cilium.GenerateManifest(tt.spec)
+	// templater tests already test whether templater.GenerateManifest returns expected values or not. This test ensures that cilium.GenerateManifest
+	// calls the templater and does not try to load the static manifest like earlier version
+	tt.h.EXPECT().Template(
+		tt.ctx, gomock.AssignableToTypeOf(""), gomock.AssignableToTypeOf(""), gomock.AssignableToTypeOf(""), gomock.AssignableToTypeOf(map[string]interface{}{}),
+	).Return([]byte("manifest"), nil)
+	_, err := tt.cilium.GenerateManifest(tt.ctx, tt.spec)
 	tt.Expect(err).To(Not(HaveOccurred()), "GenerateManifest() should succeed")
-	test.AssertContentToFile(t, string(gotFileContent), tt.spec.VersionsBundle.Cilium.Manifest.URI)
-}
-
-func TestCiliumGenerateManifestWriterError(t *testing.T) {
-	tt := newCiliumTest(t)
-	tt.spec.VersionsBundle.Cilium.Manifest.URI = "testdata/missing_manifest.yaml"
-
-	_, err := tt.cilium.GenerateManifest(tt.spec)
-	tt.Expect(err).To(MatchError(ContainSubstring("can't load networking manifest [testdata/missing_manifest.yaml]")), "GenerateManifest() should fail with missing file error")
 }
